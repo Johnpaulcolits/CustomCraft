75e72a70201b54667b2d0dc3e608c14629de9f81
10blowblow1921 <colitajohnpaul2@gmail.com>
2025-01-25 02:28:19 +0800

    update complete

--- checkout.php
+++ checkout.php
@@ -53,20 +53,35 @@
   <!--CheckOut-->
 <section class="my-5 py-5">
     <div class="container text-center mt-3 pt-5" >
-        <h2 class="form-weight-bold">Checkout</h2>
+        <h2 class="form-weight-bold">Check Out</h2>
         <hr class="mx-auto">
     </div>
     <div class="mx-auto container">
         <form action="" id="checkout-form">
-            <div class="form-group">
-                <label>Email</label>
-                <input type="text" class="form-control" id="login-email" name="email" placeholder="Email" required>
+            <div class="form-group checkout-small-element">
+                <label>Name</label>
+                <input type="text" class="form-control" id="checkout-name" name="checkout-name" placeholder="Name" required>
 
             </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="form-control" id="login-password" name="password" placeholder="Password" required>
+            <div class="form-group checkout-small-element">
+                <label>Email</label>
+                <input type="text" class="form-control" id="checkout-email" name="email" placeholder="Email" required>
                 
+            </div>
+            <div class="form-group checkout-small-element">
+              <label >Phone</label>
+               <input type="tel" class="form-control" id="checkout-phone" name="phone" placeholder="Phone" required>
+            </div>
+            <div class="form-group checkout-small-element">
+              <label>City</label>
+              <input type="text" class="form-control" id="checkout-city" name="city" placeholder="City" required>
+            </div>
+            <div class="form-group checkout-large-element">
+              <label>Address</label>
+              <input type="text" class="form-control" id="checkout-address" name="address" placeholder="Address" required>
+            </div>
+            <div class="form-group checkout-btn-container">
+              <input type="submit" class="btn" id="checkout-btn" value="Checkout">
             </div>
         </form>
     </div>
