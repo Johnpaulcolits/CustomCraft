3d25928907c7bcdcd59abd47c8255c5270df96e7
10blowblow1921 <colitajohnpaul2@gmail.com>
2025-02-02 01:11:36 +0800

    update complete

--- admin/index.php
+++ admin/index.php
@@ -35,7 +35,7 @@
 		</a>
 		<ul class="side-menu top">
 			<li>
-				<a href="index.html">
+				<a href="index.php">
 					<i class='bx bxs-dashboard' ></i>
 					<span class="text">Dashboard</span>
 				</a>
