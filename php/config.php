8a370793405b121fe2df3ba904e251b898c9ebb1
John Paul <colitajohnpaul2@gmail.com>
2025-02-25 12:15:16 +0800

    update complete

--- php/config.php
+++ php/config.php
@@ -11,8 +11,10 @@
   $password = "Cr@ft_0123!";
   $dbname = "u330866214_craft";
 
+
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
     echo "Database connection error".mysqli_connect_error();
   } 
 ?>
+  <?php ?>
